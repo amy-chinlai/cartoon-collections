b7df77dd08e91bb1e959cf52e56d3fea7a8bed3f
amy-chinlai <amy@actionnetwork.org>
2021-06-22 10:42:54 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,14 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  planeteer_calls = []
+  array.map do |name|
+    planeteer_calls.push(name.capitalize + "!")
+  end
 end
 
 def long_planeteer_calls# code an argument here
